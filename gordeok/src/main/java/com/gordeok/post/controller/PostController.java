b1190707c59c2db3589aafd9c231367e5543be02
rlagk0 <hayoung1303@gmail.com>
2026-05-20 00:21:00 +0900

    feat: 분철 글 상세 API 구현

--- gordeok/src/main/java/com/gordeok/post/controller/PostController.java
+++ gordeok/src/main/java/com/gordeok/post/controller/PostController.java
@@ -1,8 +1,10 @@
 package com.gordeok.post.controller;
 
+import com.gordeok.post.dto.PostDetailResponseDto;
 import com.gordeok.post.dto.CreatePostRequestDto;
 import com.gordeok.post.dto.CreatePostResponseDto;
 import com.gordeok.post.service.PostService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,15 +12,23 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 @RequestMapping("/api/posts")
 public class PostController {
-
     private final PostService postService;
-
+    // 분철 글 작성
     @PostMapping
     public CreatePostResponseDto createPost(
             @RequestParam Long userId,
-            @RequestBody CreatePostRequestDto request
+            @Valid @RequestBody CreatePostRequestDto request
+    ) {
+        return postService.createPost(userId, request);
+    }
+
+    // 분철 글 상세
+    @GetMapping("/{postId}")
+    public PostDetailResponseDto getPostDetail(
+            @PathVariable Long postId,
+            @RequestParam(required = false) Long userId
     ) {
 
-        return postService.createPost(userId, request);
+        return postService.getPostDetail(postId, userId);
     }
 }

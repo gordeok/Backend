b584e4c7f67029b632199ea87b459cc0ade8a29b
EunJu Jang <eunnnn31@gmail.com>
2026-05-21 13:57:47 +0900

    Delete gordeok_backend directory

--- gordeok_backend/src/main/java/com/gordeok/idol/repository/AlbumRepository.java
+++
@@ -1,7 +0,0 @@
-package com.gordeok.idol.repository;
-
-import com.gordeok.idol.entity.Album;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlbumRepository extends JpaRepository<Album, Long> {
-}
